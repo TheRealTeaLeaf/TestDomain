7495958ef7d8ba817caaf35da6e25590c21a6070
TheRealTeaLeaf <63627242+TheRealTeaLeaf@users.noreply.github.com>
2020-06-23 15:52:20 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,28 +4,28 @@ function CreateCookies()
 	var CookieName = "targetingCookie1";
 	var CookieValue = "value1";
 	var ExpDate1 = new Date();
-	ExpDate1.setTime(ExpDate1.getTime() + 1000*60*60*24*7);
+	ExpDate1.setTime(ExpDate1.getTime() + 1000*60*60*24*700);
 	var cookieStr = CookieName + "=" + CookieValue + "; " +  "path=/" + "; " + "expires=" + ExpDate1.toUTCString();
 	document.cookie = cookieStr;	
 	
 	var CookieName = "targetingCookie2";
 	var CookieValue = "value2";
 	var ExpDate2 = new Date();
-	ExpDate2.setTime(ExpDate2.getTime() + 1000*60*60*24*7);
+	ExpDate2.setTime(ExpDate2.getTime() + 1000*60*60*24*700);
 	var cookieStr = CookieName + "=" + CookieValue + "; " +  "path=/" + "; " + "expires=" + ExpDate2.toUTCString();
 	document.cookie = cookieStr;
 	
 	var CookieName = "functionalCookie1";
 	var CookieValue = "value3";
 	var ExpDate3 = new Date();
-	ExpDate3.setTime(ExpDate3.getTime() + 1000*60*60*24*7);
+	ExpDate3.setTime(ExpDate3.getTime() + 1000*60*60*24*700);
 	var cookieStr = CookieName + "=" + CookieValue + "; " +  "path=/" + "; " + "expires=" + ExpDate3.toUTCString();
 	document.cookie = cookieStr;
 	
 	var CookieName = "performanceCookie1";
 	var CookieValue = "value4";
 	var ExpDate4 = new Date();
-	ExpDate4.setTime(ExpDate4.getTime() + 1000*60*60*24*7);
+	ExpDate4.setTime(ExpDate4.getTime() + 1000*60*60*24*700);
 	var cookieStr = CookieName + "=" + CookieValue + "; " +  "path=/" + "; " + "expires=" + ExpDate4.toUTCString();
 	document.cookie = cookieStr;
 }
